7b22477d8954ea8a10d01bb4767e5c458954a442
Tim Heck <tih99924@bethel.edu>
2019-08-05 15:12:56 -0500

    nav modified, component files created

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,30 +6,49 @@ import './Nav.css';
 
 const Nav = (props) => (
   <div className="nav">
-    <Link to="/home">
-      <h2 className="nav-title">Prime Solo Project</h2>
+    <Link to="/news">
+      <img className="logo" src="images/greasysauceboss-logo-w.svg" alt="GreasySauceBoss" />
     </Link>
     <div className="nav-right">
-      <Link className="nav-link" to="/home">
+      <Link className="nav-link" to="/shows">
+        Shows
+      </Link>
+      <Link className="nav-link" to="/merch">
+        Merch
+      </Link>
+      <Link className="nav-link" to="/about">
+        Band
+      </Link>
+      <Link className="nav-link" to="/releases">
+        Releases
+      </Link>
+      <Link className="nav-link" to="/cart">
+        Your Cart
+      </Link>
+    </div>
+    
+
+    {/* <div className="nav-right">
+      <Link className="nav-link" to="/home"> */}
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
-      </Link>
+        {/* {props.user.id ? 'Home' : 'Login / Register'}
+      </Link> */}
       {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
+      {/* {props.user.id && (
         <>
           <Link className="nav-link" to="/info">
             Info Page
           </Link>
           <LogOutButton className="nav-link"/>
         </>
-      )}
+      )} */}
       {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
+      {/* <Link className="nav-link" to="/about">
         About
       </Link>
-    </div>
+    </div> */}
   </div>
 );
 
