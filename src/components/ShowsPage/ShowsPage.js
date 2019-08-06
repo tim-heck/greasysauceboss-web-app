90b59d6c25a48706a160a5ff8988ab666255d303
Tim Heck <tih99924@bethel.edu>
2019-08-06 13:11:03 -0500

    built out shows page

--- src/components/ShowsPage/ShowsPage.js
+++ src/components/ShowsPage/ShowsPage.js
@@ -3,13 +3,32 @@ import { connect } from 'react-redux';
 
 class ShowsPage extends Component {
 
+    checkTickets = (show) => {
+        if (show.ticket) {
+            <a href={show.ticket_url}>Tickets</a>
+        }
+    }
+
     render() {
         return (
             <>
-
+                <h2>Upcoming Shows</h2>
+                <ul>
+                    {this.props.reduxStore.showsReducer.map(item =>
+                        <li key={item.id}>
+                            <h3>{item.show_date}</h3>
+                            <p>{item.location}</p>
+                            {this.checkTickets(item)}
+                        </li>
+                    )}
+                </ul>
             </>
         );
     }
 }
 
-export default connect()(ShowsPage);
+const stateToProps = (reduxStore) => ({
+    reduxStore
+})
+
+export default connect(stateToProps)(ShowsPage);
