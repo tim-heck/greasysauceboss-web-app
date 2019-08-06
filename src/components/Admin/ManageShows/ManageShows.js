4a949f2bd2277953d68a063deeb9d0efcf0d5493
Tim Heck <tih99924@bethel.edu>
2019-08-06 15:50:36 -0500

    built out manage shows page

--- src/components/Admin/ManageShows/ManageShows.js
+++ src/components/Admin/ManageShows/ManageShows.js
@@ -3,13 +3,39 @@ import { connect } from 'react-redux';
 
 class ManageShows extends Component {
 
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_SHOWS' });
+    }
+
+    handleClick = (showToModify, dispatchType) => {
+        if (dispatchType === 'edit') {
+            this.props.history.push('/shows-form');
+        } else {
+            this.props.dispatch({ type: 'DELETE_SHOW', payload: showToModify })
+        }
+    }
+
     render() {
         return (
             <>
-
+                <h2>Manage Shows</h2>
+                <ul>
+                    {this.props.reduxStore.shows.map(item => 
+                        <li key={item.id}>
+                            <h3>{item.show_date}</h3>
+                            <p>{item.location}</p>
+                            <button onClick={() => this.handleClick(item, 'edit')}>Edit</button>
+                            <button onClick={() => this.handleClick(item, 'delete')}>Delete</button>
+                        </li>
+                    )}
+                </ul>
             </>
         );
     }
 }
 
-export default connect()(ManageShows);
+const stateToProps = (reduxStore) => ({
+    reduxStore
+});
+
+export default connect(stateToProps)(ManageShows);
