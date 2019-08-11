0a829c9f584ce02baf382aa0ab892a88407b5f85
Tim Heck <tih99924@bethel.edu>
2019-08-11 10:10:37 -0500

    manage shows page styled

--- src/components/Admin/ManageShows/ManageShows.js
+++ src/components/Admin/ManageShows/ManageShows.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    button: {
+        fontFamily: "Montserrat, sans-serif",
+        margin: theme.spacing.unit,
+        backgroundColor: "#1f2833",
+        color: "#fff",
+        padding: "10px 25px",
+        letterSpacing: "1.5px",
+        fontWeight: "700",
+        transition: ".3s",
+        '&:hover': {
+            backgroundColor: "#fff",
+            color: "#1f2833",
+            boxShadow: "0px 0px 0px 3px #66fcf1",
+        }
+    }
+});
 
 class ManageShows extends Component {
 
@@ -21,29 +41,34 @@ class ManageShows extends Component {
     }
 
     checkTickets = (show) => {
+        const { classes } = this.props;
         if (show.ticket) {
             return (
-                <a href={show.ticket_url}>Tickets</a>
+                <Button variant="contained" href={show.ticket_url} className={classes.button} target="_blank">Tickets</Button>
             );
         }
     }
 
     render() {
+        const { classes } = this.props;
         return (
             <>
-                <h2>Manage Shows</h2>
-                <ul>
-                    {this.props.reduxStore.shows.showsReducer.map(item => 
-                        <li key={item.id}>
-                            <h3>{item.show_date}</h3>
-                            <p>{item.location}</p>
-                            {this.checkTickets(item)}
-                            <button onClick={() => this.handleClick('edit', item)}>Edit</button>
-                            <button onClick={() => this.handleClick('delete', item)}>Delete</button>
-                        </li>
-                    )}
-                </ul>
-                <button onClick={() => this.handleClick('add')}>Add New Show</button>
+                <div className="container shows-list">
+                    <h2 className="page-title">Manage Shows</h2>
+                    <ul>
+                        {this.props.reduxStore.shows.showsReducer.map(item =>
+                            <li key={item.id}>
+                                <h3 className="show-item show-date">{item.show_date}</h3>
+                                <div className="show-item location">{item.location}</div>
+                                <div className="show-item ticket">{this.checkTickets(item)}</div>
+                                <Button variant="contained" className={classes.button} onClick={() => this.handleClick('edit', item)}>Edit</Button>
+                                <Button variant="contained" className={classes.button} onClick={() => this.handleClick('delete', item)}>Delete</Button>
+                            </li>
+                        )}
+                    </ul>
+                    <br />
+                    <Button variant="contained" className={classes.button} onClick={() => this.handleClick('add')}>Add New Show</Button>
+                </div> 
             </>
         );
     }
@@ -53,4 +78,4 @@ const stateToProps = (reduxStore) => ({
     reduxStore
 });
 
-export default connect(stateToProps)(ManageShows);
+export default withStyles(styles)(connect(stateToProps)(ManageShows));
